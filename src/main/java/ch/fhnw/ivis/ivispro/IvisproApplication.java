e31afb88bef9fc558bfaa79489efef008ee5a645
Kevin Kirn <kevin.kirn@students.fhnw.ch>
2018-05-29 17:41:28 +0200

    inital commit

---
+++ src/main/java/ch/fhnw/ivis/ivispro/IvisproApplication.java
@@ -0,0 +1,12 @@
+package ch.fhnw.ivis.ivispro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvisproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvisproApplication.class, args);
+	}
+}
